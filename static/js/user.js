bc31278d008451edbf8634ed5146984ee454254a
sally <lujjiang@cn.ibm.com>
2016-03-26 23:31:02 +0800

    typo

--- static/js/user.js
+++ static/js/user.js
@@ -122,7 +122,7 @@ $(function () {
         $(text).find('.media-left a').attr('href','/users/'+item._id);
         var Arr = ["a6-160","a7-160","a8-160","a9-160","a10-160","a11-160","a12-160","a13-160","a14-160","a15-160"];
         var n = Math.floor(Math.random() * Arr.length + 1)-1;
-        $(text).find('img.media-objects').attr('src',user.logo_img?user.logo_img:'/images/avatars/'+Arr[n]+'.png');
+        $(text).find('img.media-object').attr('src',user.logo_img?user.logo_img:'/images/avatars/'+Arr[n]+'.png');
         $(text).find('.media-heading a').attr('href','/users/'+item._id);
         $(text).find('.media-heading a strong').text(user.name?user.name:(user.email?user.email.split('@')[0]:(user.mobile_phone?user.mobile_phone.toString().substring(0,3)+"****"+user.mobile_phone.toString().substring(8,11):"ID:"+user.id)));
         $(text).find('.media-heading small').text(user.role);
